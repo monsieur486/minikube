9163b8ad78c99e445235df18b13f209dc88cc266
monsieur486 <monsieur486@gmail.com>
2021-02-02 16:47:44 +0100

    first commit

--- src/main/java/com/mr486/microservice/controllers/DummyController.java
+++ src/main/java/com/mr486/microservice/controllers/DummyController.java
@@ -3,12 +3,17 @@ package com.mr486.microservice.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class DummyController {
 
   @GetMapping("/")
-  public String hello(){
-    return "Coucou ma Darling !!! Ca marche";
+  public String hello() throws UnknownHostException {
+    return "<h1>Coucou ma Darling !!! Ca marche :"
+      + InetAddress.getLocalHost().getHostName()
+      +"</h1>";
   }
 
 }
